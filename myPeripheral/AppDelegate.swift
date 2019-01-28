f544729e10ae59e380cbcb95a907499d024f469c
nguyenlam455 <livefortoday01455@gmail.com>
2019-01-29 00:10:17 +0700

    Initial Commit

---
+++ myPeripheral/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  myPeripheral
+//
+//  Created by Nguyen Lam on 1/29/19.
+//  Copyright © 2019 Nguyen Lam. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
